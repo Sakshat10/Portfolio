03ac25b1fae18b4d5a39df03ebde76d67d587dcf
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-06 23:37:19 +0530

    refactor Approach component layout and styling

--- components/main/Approach.tsx
+++ components/main/Approach.tsx
@@ -6,13 +6,13 @@ import { CanvasRevealEffect } from "../ui/CanvasRevealEffect";
 
 const Approach = () => {
   return (
-    <section className="w-full py-20">
+    <section className="py-20">
       <h1 className="heading text-white">
         My <span className="text-transparent bg-clip-text bg-gradient-to-b from-purple-500 to-cyan-500">approach</span>
       </h1>
-      {/* remove bg-white dark:bg-black */}
+
       <div className="my-20 flex flex-col lg:flex-row items-center text-white justify-center w-full gap-4">
-        {/* add des prop */}
+
         <Card
           title="Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
@@ -22,7 +22,6 @@ const Approach = () => {
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
-            // add these classed for the border rounded overflowing -> rounded-3xl overflow-hidden
             containerClassName="bg-emerald-900 rounded-3xl overflow-hidden"
           />
         </Card>
@@ -87,7 +86,7 @@ const Card = ({
       onMouseLeave={() => setHovered(false)}
       // change h-[30rem] to h-[35rem], add rounded-3xl
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center
-       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl "
+       dark:border-white/[0.2]  max-w-lg w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl "
       style={{
         //   add these two
         //   you can generate the color from here https://cssgradient.io/
